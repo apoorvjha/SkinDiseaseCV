c93205ef68b3a1cff39e5138b0043c15ba0c5a1a
TensorNd <do_not_contact@this_email.lol>
2021-12-01 02:24:13 +0530

    Rectified issue with model initialization.

--- Model.py
+++ Model.py
@@ -16,10 +16,7 @@ padding='same',input_shape=properties.input_shape,strides=properties.stride[0],
 dilation_rate=properties.dilation[0]))
 model.add(MaxPool2D(pool_size=properties.pool_size[0]))
 for i in range(1,len(properties.filters)):
-    model.add(Conv2D(filters=properties.filters[i],
-    kernel_size=properties.kernel_size[i],activation='relu',
-    padding='same',input_shape=properties.input_shape[i],strides=properties.stride[i],
-    dilation_rate=properties.dilation[i]))
+    model.add(Conv2D(filters=properties.filters[i],kernel_size=properties.kernel_size[i],activation='relu',padding='same',strides=properties.stride[i],dilation_rate=properties.dilation[i]))
     model.add(MaxPool2D(pool_size=properties.pool_size[i]))
 model.add(Flatten())
 for i in range(len(properties.dense_layer_neurons)):
