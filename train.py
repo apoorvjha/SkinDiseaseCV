10527b244dcd8f0c6a67dfc729120aa406f91e1b
TensorNd <do_not_contact@this_email.lol>
2021-12-03 02:26:35 +0530

    Added check for presence of model file for supporting incremental training and rectified feature extraction code

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 import Model
 import DataAugmentation
 import properties
+from sklearn.model_selection import train_test_split
+import os
 
 path='./static/Dataset/'
 X=[]
@@ -10,10 +12,16 @@ for i in range(len(directories)):
     image=DataAugmentation.Augment(path+directories[i]+'/')
     for j in range(image.shape[0]):
         Y.append(directories[i])
-    X.extend(image)
+        X.append(image[j])
 X=Model.array(X)
 Y=Model.Class2OHV(Model.array(Y))
-model=Model.instantiateModel()
-model,history=Model.fit(X,Y,model)
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=properties.test_ratio,shuffle=properties.shuffle,random_state=properties.random_state)
+if os.path.exists(properties.model_name):
+    model=Model.load()
+else: 
+    model=Model.instantiateModel()
+model,history=Model.fit(X_train,Y_train,model)
 Model.save(model)    
 Model.plot_train_history(history)
+predictions=Model.predict(X_test, model)
+print("Accuracy = "+str(Model.accuracy(predictions, Y_test)))
