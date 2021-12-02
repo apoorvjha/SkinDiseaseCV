2527d1f60e5a2125a1b8abab76f30ad4e417e8d1
TensorNd <do_not_contact@this_email.lol>
2021-12-02 19:41:36 +0530

    Removed global variable assignments and made then local to Augment function.

--- DataAugmentation.py
+++ DataAugmentation.py
@@ -3,17 +3,14 @@ from os import listdir
 from random import randint
 import properties
 
-rotation_range=properties.rotation_range
-steps=properties.steps
-dimension=properties.dimension
 
 def Augment(source_path):
     images=[]
     for i in listdir(source_path):
         image=driver.read_image(source_path+i)
-        image=driver.Resize(image,dimension=dimension)
+        image=driver.Resize(image,dimension=properties.dimension)
         skin=driver.SkinDetection(image)
-        rotated_images=driver.Rotate(skin,rotation=rotation_range,steps=steps)
+        rotated_images=driver.Rotate(skin,rotation=properties.rotation_range,steps=properties.steps)
         brightness_adjusted_images=driver.AdjustBrightness(skin)
         flipped_images=driver.FlipImage(skin)
         sharp_image=driver.Sharpening(skin)
