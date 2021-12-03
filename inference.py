15d01b02538136917ad3ab07e675b2b7a08e45be
TensorNd <do_not_contact@this_email.lol>
2021-12-04 00:39:31 +0530

    Script to make inferences

---
+++ inference.py
@@ -0,0 +1,25 @@
+import Model
+import ImageProcessing as driver
+import properties
+import os
+
+path='./static/processing_dir/'
+X=[]
+for i in os.listdir(path):
+    image=driver.read_image(path+i)
+    image=driver.Resize(image,dimension=properties.dimension)
+    image=driver.SkinDetection(image)
+    X.append(image)
+X=Model.array(X)
+if os.path.exists(properties.model_name):
+    model=Model.load()
+else: 
+    import train
+    model=Model.load()
+predictions=Model.OHV2Class(Model.predict(X,model))
+for i in range(len(os.listdir(path))):
+    print(f"{os.listdir(path)[i]} belongs to {list(properties.classes.keys())[predictions[i]]}")
+
+   
+
+
