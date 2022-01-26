4e7b5784b618eeb53745700d2d16cb79a97a9e3b
TensorNd <do_not_contact@this_email.lol>
2022-01-26 16:57:27 +0530

    Added routes for fetching dataset and metadata about the model and getting inferences for supplied images

--- web_backend.py
+++ web_backend.py
@@ -2,6 +2,14 @@ from flask import Flask, render_template, redirect, flash, request, url_for, ses
 from werkzeug.utils import secure_filename
 import json
 import Auth
+import time
+import Model
+import ImageProcessing as driver
+import DataAugmentation
+import properties
+import os
+from os import listdir
+from sklearn.model_selection import train_test_split
 
 app=Flask(__name__)
 app.secret_key="qazwsx@2022"
@@ -137,6 +145,78 @@ def changeProfPic():
             profilePic.save(fileName)
             flash("Profile pic changed successfully!","alert alert-success")
             return redirect(url_for('index'))
+        else:
+            return {"status" : 404}
+    else:
+        return {"status" : 500}
+
+@app.route('/inference',methods=['POST'])
+def inference():
+    if session['userId']:
+        if request.method=='POST':
+            image=request.files['file']	
+            fileName=str(session['userId']) + secure_filename(image.filename)
+            image.save('static/processing_dir/'+fileName)
+            start=time.time()
+            X=[]
+            image_cv=driver.read_image('static/processing_dir/'+fileName)
+            image_cv=driver.Resize(image_cv,dimension=properties.dimension)
+            image_cv=driver.SkinDetection(image_cv)
+            X.append(image_cv)
+            X=Model.array(X)
+            model=Model.load()
+            predictions=Model.OHV2Class(Model.predict(X,model))
+            time_taken=time.time() - start
+            return {"image" : 'static/processing_dir/'+fileName,"prediction" : list(properties.classes.keys())[predictions[0]], "status" : 200,"response_time" : round(time_taken,3)}
+        else:
+            return {"status" : 404}
+    else:
+        return {"status" : 500}
+@app.route('/getModelStats')
+def getModelStats():
+    if session['userId']:
+        if request.method=='GET':
+            response={"params" : [],"status" : 200}
+            response["params"].append({"name" : "rotation_range","value" : properties.rotation_range})
+            response["params"].append({"name" : "steps","value" : properties.steps})
+            #response["params"].append({"name" : "dimension","value" : properties.dimension})
+            #response["params"].append({"name" : "kernel_size","value" : properties.kernel_size})
+            #response["params"].append({"name" : "input_shape","value" : properties.input_shape})
+            #response["params"].append({"name" : "stride","value" : properties.stride})
+            #response["params"].append({"name" : "dilation","value" : properties.dilation})
+            #response["params"].append({"name" : "pool_size","value" : properties.pool_size})
+            response["params"].append({"name" : "n_output","value" : properties.n_output})
+            response["params"].append({"name" : "dropout_probability","value" : properties.dropout_probability})
+            response["params"].append({"name" : "learning_rate","value" : properties.learning_rate})
+            response["params"].append({"name" : "batch_size","value" : properties.batch_size})
+            response["params"].append({"name" : "epochs","value" : properties.epochs})
+            response["params"].append({"name" : "model_name","value" : properties.model_name})
+            response["params"].append({"name" : "validation_split","value" : properties.validation_split})
+            response["params"].append({"name" : "test_ratio","value" : properties.test_ratio})
+            response["params"].append({"name" : "random_state","value" : properties.random_state})
+            response["params"].append({"name" : "verbose","value" : properties.verbose})
+            #response["params"].append({"name" : "classes","value" : properties.classes})
+            response["params"].append({"name" : "precision","value" : properties.precision})
+            response["params"].append({"name" : "shuffle","value" : properties.shuffle})
+            response["params"].append({"name" : "decay_steps","value" : properties.decay_steps})
+            response["params"].append({"name" : "decay_rate","value" : properties.decay_rate})
+            return response
+        else:
+            return {"status" : 404}
+    else:
+        return {"status" : 500}
+
+@app.route('/getDataset')
+def getDataset():
+    if session['userId']:
+        if request.method=='GET':
+            response={"datasets" : [],"status" : 200}
+            path='./static/Dataset/'
+            directories=list(properties.classes.keys())
+            for i in range(len(directories)):
+                for j in listdir(path+directories[i]+'/'):
+                    response["datasets"].append({"name" : directories[i],"image" : path+directories[i]+'/'+j})
+            return response
         else:
             return {"status" : 404}
     else:
